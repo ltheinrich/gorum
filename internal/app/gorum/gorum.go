164d93d3599e6faebc942b73465eb1b95c8182f4
Lennart Heinrich <lennart@ltheinrich.de>
2018-10-11 16:14:14 +0200

    add db connection

--- internal/app/gorum/gorum.go
+++ internal/app/gorum/gorum.go
@@ -2,11 +2,16 @@ package gorum
 
 import (
 	"github.com/lheinrichde/golib/pkg/config"
+	"github.com/lheinrichde/golib/pkg/db"
 )
 
 // Init startup
 func Init() error {
 	if err := loadConfig(); err != nil {
+		return err
+	}
+
+	if err := connectDB(); err != nil {
 		return err
 	}
 
@@ -25,4 +30,15 @@ func loadConfig() error {
 	}
 
 	return nil
+}
+
+func connectDB() error {
+	host := config.Get("postgresql", "host")
+	port := config.Get("postgresql", "port")
+	ssl := config.Get("postgresql", "ssl")
+	database := config.Get("postgresql", "database")
+	username := config.Get("postgresql", "username")
+	password := config.Get("postgresql", "password")
+
+	return db.Connect(host, port, ssl, database, username, password)
 }
