16fc7b71d64a3d676e10341738aee7ae83de0032
Lennart Heinrich <lennart@ltheinrich.de>
2018-10-23 18:45:02 +0200

    add installer.sh and add 386 to build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ GOOS=linux GOARCH=arm GOARM=6 go build -o bin/gorum-linux-armv6 cmd/gorum/gorum.
 # linux armv7
 GOOS=linux GOARCH=arm GOARM=7 go build -o bin/gorum-linux-armv7 cmd/gorum/gorum.go
 
+# linux 386
+GOOS=linux GOARCH=386 go build -o bin/gorum-linux-386 cmd/gorum/gorum.go
+
 # build web-app
 cd web
 ng build --prod
