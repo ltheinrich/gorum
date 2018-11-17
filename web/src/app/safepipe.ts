e759e5693259469b27961d99f10bbd510369e206
Lennart Heinrich <ltheinrich@evortex.de>
2018-11-17 17:04:53 +0100

    users view -> display no users exist

--- web/src/app/safepipe.ts
+++ web/src/app/safepipe.ts
@@ -3,7 +3,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({ name: 'safe' })
 export class SafePipe implements PipeTransform {
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer) { }
   transform(url) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
