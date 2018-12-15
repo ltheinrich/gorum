a63e0ff708cd5e493fa1b2240945e52be8d4ab04
Lennart Heinrich <lennart@ltheinrich.de>
2018-12-15 18:11:44 +0100

    http.sh use always POST method

--- http.sh
+++ http.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-METHOD=$1
-URL=$2
-DATA=$3
+URL=$1
+DATA=$2
 
-if [ $1 != "POST" ] && [ $1 != "GET" ] && [ $1 != "PUT" ] && [ $1 != "DELETE" ]
-    then
-        METHOD="POST"
-        URL=$1
-        DATA=$2
-fi
-
-curl -k -i -H 'Content-Type: application/json' -X $METHOD -d "$DATA" https://localhost:1813/$URL
+curl -k -i -H 'Content-Type: application/json' -X POST -d "$DATA" https://localhost:1813/$URL
 echo
