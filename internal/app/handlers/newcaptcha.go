ba36f62b11fbf3b7ff5a7bdee4047ec6908b3501
Lennart Heinrich <lennart@ltheinrich.de>
2018-12-15 19:47:19 +0100

    captcha option in config

--- internal/app/handlers/newcaptcha.go
+++ internal/app/handlers/newcaptcha.go
@@ -1,11 +1,20 @@
 package handlers
 
 import (
+	"errors"
+
 	"github.com/dchest/captcha"
+	"github.com/lheinrichde/gorum/pkg/config"
 )
 
 // NewCaptcha handler
 func NewCaptcha(request map[string]interface{}, username string, auth bool) interface{} {
+	// check if captcha enabled
+	if config.Get("https", "captcha") != "true" {
+		// not enabled
+		return errors.New("501")
+	}
+
 	// generate captcha and respond
 	cap := captcha.New()
 	return map[string]interface{}{"captcha": cap}
