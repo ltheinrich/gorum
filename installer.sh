57e21ecee668e612063d138691982b7736566a28
Lennart Heinrich <ltheinrich@evortex.de>
2018-12-15 18:34:53 +0100

    installer.sh automatically detect architecture

--- installer.sh
+++ installer.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-if [ "$1" != "" ] && [ "$2" != "" ]; then
+ARCH=$(arch)
+ARCHITECTURE=""
+
+if [ $ARCH == "x86_64" ]; then
+    ARCHITECTURE="amd64"
+elif [ $ARCH == "armv8*" ] || [ $ARCH == "arm64*" ]; then
+    ARCHITECTURE="arm64"
+elif [ $ARCH == "armv7*" ]; then
+    ARCHITECTURE="armv7"
+elif [ $ARCH == "arm*" ]; then
+    ARCHITECTURE="armv6"
+elif [ $ARCH == "i*86" ]; then
+    ARCHITECTURE="386"
+else
+    echo "unsupported architecture"
+    exit
+fi
+
+if [ "$1" != "" ]; then
         # executable
-        wget -O gorum-linux https://github.com/lheinrichde/gorum/releases/download/v$1/gorum-linux-$2
+        wget -O gorum-linux https://github.com/lheinrichde/gorum/releases/download/v$1/gorum-linux-$ARCHITECTURE
         chmod +x gorum-linux
 
         # resources
         wget https://github.com/lheinrichde/gorum/releases/download/v$1/resources.tar.gz
         tar xfvz resources.tar.gz
 else
-    echo "use ./installer.sh VERSION(x.y.z) ARCHITECTURE(amd64,arm64,armv6,armv7,386)"
+    echo "use ./installer.sh VERSION(x.y.z)"
 fi
