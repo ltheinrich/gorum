7967bd0a234025ceab9fbe3d4d21066e24596773
Lennart Heinrich <lennart@ltheinrich.de>
2018-12-18 17:55:20 +0100

    add comment to mailregex and optimize readme

--- pkg/tools/tools.go
+++ pkg/tools/tools.go
@@ -4,4 +4,5 @@ import (
 	"regexp"
 )
 
+// MailRegEx to validate mail address
 var MailRegEx = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
