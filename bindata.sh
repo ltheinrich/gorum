5324a937d0989c15d146a7ef0f2489d65c33d479
Lennart Heinrich <lennart@ltheinrich.de>
2018-12-22 12:45:44 +0100

    binary data in update.sh

--- bindata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd assets
-go-bindata -o ../internal/pkg/assets/assets.go -pkg assets .
-cd ..
--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# dependencies
 go get -u ./...
 go mod vendor
+
+# binary data
+cd assets
+go-bindata -o ../internal/pkg/assets/assets.go -pkg assets .
+cd ..
