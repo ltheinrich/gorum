a7ec02000aec445a6975a7b7403c007a342805eb
Lennart Heinrich <lennart@ltheinrich.de>
2019-01-08 18:54:45 +0100

    reformat build script

--- build.sh
+++ build.sh
@@ -6,27 +6,27 @@ npm install
 ng build --prod
 cd ..
 
-# assets binary data
-cd assets
-go-bindata -o ../internal/pkg/assets/assets.go -pkg assets .
-cd ..
-
 # web assets binary data
 cd web/dist/gorum
 go-bindata -o ../../../internal/pkg/webassets/webassets.go -pkg webassets .
 cd ../../..
 
+# assets binary data
+cd assets
+go-bindata -o ../internal/pkg/assets/assets.go -pkg assets .
+cd ..
+
 # linux amd64
-GOOS=linux GOARCH=amd64 go build -o bin/gorum-linux-amd64 main.go
+GOOS=linux GOARCH=amd64 go build -o bin/gorum-linux-amd64
 
 # linux arm64
-GOOS=linux GOARCH=arm64 go build -o bin/gorum-linux-arm64 main.go
+GOOS=linux GOARCH=arm64 go build -o bin/gorum-linux-arm64
 
 # linux armv6
-GOOS=linux GOARCH=arm GOARM=6 go build -o bin/gorum-linux-armv6 main.go
+GOOS=linux GOARCH=arm GOARM=6 go build -o bin/gorum-linux-armv6
 
 # freebsd amd64
-GOOS=freebsd GOARCH=amd64 go build -o bin/gorum-freebsd-amd64 main.go
+GOOS=freebsd GOARCH=amd64 go build -o bin/gorum-freebsd-amd64
 
 # freebsd armv6
-GOOS=freebsd GOARCH=arm GOARM=6 go build -o bin/gorum-freebsd-armv6 main.go
+GOOS=freebsd GOARCH=arm GOARM=6 go build -o bin/gorum-freebsd-armv6
