021dd7b62300a65ec1ab156663a42146130afc1b
Lennart Heinrich <ltheinrich@evortex.de>
2019-01-11 15:13:25 +0100

    official builds only for linux-amd64 and freebsd-amd64

--- build.sh
+++ build.sh
@@ -22,14 +22,5 @@ cd ..
 # linux amd64
 GOOS=linux GOARCH=amd64 go build -o bin/gorum-linux-amd64
 
-# linux arm64
-GOOS=linux GOARCH=arm64 go build -o bin/gorum-linux-arm64
-
-# linux armv6
-GOOS=linux GOARCH=arm GOARM=6 go build -o bin/gorum-linux-armv6
-
 # freebsd amd64
 GOOS=freebsd GOARCH=amd64 go build -o bin/gorum-freebsd-amd64
-
-# freebsd armv6
-GOOS=freebsd GOARCH=arm GOARM=6 go build -o bin/gorum-freebsd-armv6
