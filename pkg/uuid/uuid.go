a77e58893e56a913974ffce387db4a8cec21482d
Lennart Heinrich <lennart@ltheinrich.de>
2019-01-12 13:14:58 +0100

    faster uuid

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -3,27 +3,10 @@ package uuid
 import (
 	"crypto/rand"
 	"fmt"
-
-	"github.com/ltheinrich/gorum/pkg/crypter"
 )
 
-// Generate return UUID as string (with hashing)
+// Generate return UUID as string
 func Generate() string {
-	// make byte slice
-	random := make([]byte, 64)
-
-	// read from random
-	rand.Read(random)
-
-	// hash
-	hashed := crypter.HashByte(random)
-
-	// return uuid as string
-	return fmt.Sprintf("%x-%x-%x-%x-%x", hashed[2:6], hashed[8:10], hashed[12:14], hashed[19:21], hashed[25:31])
-}
-
-// Unsafe return UUID as string (without hashing)
-func Unsafe() string {
 	// make byte slice
 	random := make([]byte, 16)
 
