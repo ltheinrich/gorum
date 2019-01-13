e620dda81b366ea16299433af3b3651c2731c9cb
Lennart Heinrich <lennart@ltheinrich.de>
2019-01-13 14:21:34 +0100

    optimize scripts

--- build.sh
+++ build.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-# build web-app
 cd web
-rm -rf dist
-rm -rf node_modules
-rm -f package-lock.json
-npm install
-ng build --prod
+rm -rf dist; rm -rf node_modules; rm -f package-lock.json
+npm install && ng build --prod
 
-# web assets binary data
-cd dist/gorum
-go-bindata -o ../../../internal/pkg/webassets/webassets.go -pkg webassets .
+cd dist/gorum && go-bindata -o ../../../internal/pkg/webassets/webassets.go -pkg webassets .
+cd ../../../assets && go-bindata -o ../internal/pkg/assets/assets.go -pkg assets .
 
-# assets binary data
-cd ../../../assets
-go-bindata -o ../internal/pkg/assets/assets.go -pkg assets .
-
-# build binaries and sign
-rm -rf ../bin
-GOOS=linux GOARCH=amd64 go build -o ../bin/gorum-linux-amd64
-GOOS=freebsd GOARCH=amd64 go build -o ../bin/gorum-freebsd-amd64
-cd ../bin
-gpg2 -a --detach-sign gorum-linux-amd64
-gpg2 -a --detach-sign gorum-freebsd-amd64
+cd .. && rm -rf bin
+GOOS=linux GOARCH=amd64 go build -o bin/gorum-linux-amd64
+GOOS=freebsd GOARCH=amd64 go build -o bin/gorum-freebsd-amd64
+cd bin && gpg2 -a --detach-sign gorum-linux-amd64 && gpg2 -a --detach-sign gorum-freebsd-amd64
