e12dc3143cc53b94d52fe345fe6945812460d5d7
Lennart Heinrich <ltheinrich@evortex.de>
2019-01-14 16:28:26 +0100

    only linux (amd64) builds by default

--- build.sh
+++ build.sh
@@ -7,7 +7,5 @@ npm install && ng build --prod
 cd dist/gorum && go-bindata -o ../../../internal/pkg/webassets/webassets.go -pkg webassets .
 cd ../../../assets && go-bindata -o ../internal/pkg/assets/assets.go -pkg assets .
 
-cd .. && rm -rf bin
-GOOS=linux GOARCH=amd64 go build -o bin/gorum-linux-amd64
-GOOS=freebsd GOARCH=amd64 go build -o bin/gorum-freebsd-amd64
-cd bin && gpg2 -a --detach-sign gorum-linux-amd64 && gpg2 -a --detach-sign gorum-freebsd-amd64
+cd .. && rm -f gorum; rm -f gorum.asc;
+GOOS=linux GOARCH=amd64 go build && gpg2 -a --detach-sign gorum
