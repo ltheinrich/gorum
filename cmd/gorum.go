6bde1bb4613cda5a9d50dd0d5656265bc5b84d2e
Lennart Heinrich <lennart@ltheinrich.de>
2019-02-05 18:52:53 +0100

    safe TLS configuration for HTTPS

--- cmd/gorum.go
+++ cmd/gorum.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
@@ -132,5 +133,23 @@ func listen() error {
 
 	// https/tls server
 	log.Printf("Webserver listening at https://%v/\n", url)
-	return http.ListenAndServeTLS(address, certificate, key, nil)
+	httpsServer := &http.Server{Addr: address,
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
+			CurvePreferences: []tls.CurveID{
+				tls.X25519,
+				tls.CurveP256,
+				tls.CurveP384,
+				tls.CurveP521,
+			},
+		}}
+	return httpsServer.ListenAndServeTLS(certificate, key)
 }
