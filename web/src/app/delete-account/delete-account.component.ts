c609f0d4af383d79b2e488787d1e7caeb18f9b56
Nathan <nathan.nr@outlook.com>
2019-02-10 01:28:29 +0100

    add delete account in frontend (ltheinrich/gorum#13)

---
+++ web/src/app/delete-account/delete-account.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {Config} from '../config';
+import {Title} from '@angular/platform-browser';
+import {User} from '../user/user.component';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-delete-account',
+  templateUrl: './delete-account.component.html',
+  styleUrls: ['./delete-account.component.css']
+})
+export class DeleteAccountComponent implements OnInit {
+  config = Config;
+  conf = Config.get;
+  lang = Config.lang;
+
+  user = new User(0, {});
+  password = '';
+
+  constructor(private title: Title, private router: Router) {}
+
+  ngOnInit() {
+    Config.setLogin(this.title, 'deleteAccount', true, null);
+  }
+
+  deleteAccount() {
+    if (this.password.trim() === '') {
+      Config.openSnackBar(Config.lang('fillAllFields'));
+    } else if (Config.hash(this.password) !== localStorage.getItem('password')) {
+      Config.openSnackBar(Config.lang('wrongPassword'));
+    } else {
+      Config.API('deleteaccount', {
+        username: localStorage.getItem('username'), password: localStorage.getItem('password')
+      }).subscribe(values => values['success'] === true ? this.accountDeleted() :
+        values['error'] === '403' ? Config.openSnackBar(Config.lang('wrongPassword')) : Config.openSnackBar('error'));
+    }
+  }
+
+  accountDeleted() {
+    localStorage.setItem('password', null);
+    Config.openSnackBar(Config.lang('accountDeleted'));
+    this.router.navigate(['/']);
+  }
+}
